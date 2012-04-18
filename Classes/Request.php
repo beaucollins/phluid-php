0010246675b75c893caf3438ccdd037f1f92fcda
Beau Collins <beaucollins@gmail.com>
2012-04-18 15:44:54 -0700

    Adding regex to work with .:format in path patterns

--- Classes/Request.php
+++ Classes/Request.php
@@ -73,7 +73,8 @@ class Request {
    */
   public function compileRegex( $pattern ){
     // sorry about the magic here
-    $regex_pattern = preg_replace( "#(/)?:([\w]+)(\?)?#", '($1(?<$2>[^/]+))$3', $pattern );
+    $regex_pattern = preg_replace( "/\.:/", "\\.:", $pattern );
+    $regex_pattern = preg_replace( "#(/)?:([\w]+)(\?)?#", '($1(?<$2>[^/]+))$3', $regex_pattern );
     return '#' . $regex_pattern . '#';
   }
   
