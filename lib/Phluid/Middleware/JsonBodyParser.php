3e27bdada0ba1df723eee4cc813d77ae950e359e
Beau Collins <beaucollins@gmail.com>
2012-10-21 08:08:10 -0700

    Adding more JsonBodyParser intialization configuration

--- lib/Phluid/Middleware/JsonBodyParser.php
+++ lib/Phluid/Middleware/JsonBodyParser.php
@@ -4,20 +4,20 @@ namespace Phluid\Middleware;
 
 class JsonBodyParser {
   
-  private $array;
+  private $array = true;
+  private $json_decode_options = 0; // JSON_BIGINT_AS_STRING option
+  private $depth = 512;
   
-  function __construct( $array = true ){
-    $this->array = $array;
+  function __construct( $as_assoc_array = true, $depth = 512, $json_decode_options = 0 ){
+    $this->array = $as_assoc_array;
+    $this->depth = $depth;
+    $this->options = $json_decode_options;
   }
   
   //Just JSON
   function __invoke( $request, $response, $next ){
     if ( strpos( $request->getBody(), "{") === 0 || $request->getHeader('Content-Type') == 'application/json' ) {
-      try {
-        $request->setBody( json_decode( $request->getBody(), $this->array ) );
-      } catch (Exception $e) {
-        
-      }
+      $request->setBody( json_decode( $request->getBody(), $this->array ) );
     }
     $next();
   }
