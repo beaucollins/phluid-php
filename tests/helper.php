35416ae33f126185778a6633f80c27ee26199baf
Beau Collins <beaucollins@gmail.com>
2012-12-20 13:28:14 -0800

    The autoloader is a singleton so it can be loaded multiple times Fixes a problem where the autoloader has been loaded before the test helper

--- tests/helper.php
+++ tests/helper.php
@@ -1,5 +1,5 @@
 <?php
 
-$autoload = require_once(__DIR__ . '/../vendor/autoload.php');
+$autoload = require( dirname(__DIR__) . '/vendor/autoload.php' );
 $autoload->add( 'Phluid\\Test', __DIR__ );
 
