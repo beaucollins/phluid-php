420db2fba5be57bd0609c42cf16e26af3cae263b
Beau Collins <beaucollins@gmail.com>
2012-12-20 13:18:13 -0800

    Adding multiple examples

--- example/server.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once( realpath( '../' ) . '/vendor/autoload.php' );
-
-$app = require( 'App.php' );
-
-$app->listen( 4000 );
---
+++ example/site/server.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once( realpath( '../../' ) . '/vendor/autoload.php' );
+
+$app = require( 'App.php' );
+
+$app->listen( 4000 );
